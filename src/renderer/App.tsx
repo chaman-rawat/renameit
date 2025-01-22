db021b4ee0bfa78420570a9c72c2611db765613a
Chaman Rawat <chamanrawat.dev@gmail.com>
2025-01-22 11:55:08 +0530

    feat: add tailwindcss

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,39 +1,16 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import icon from '../../assets/icon.svg';
+import 'tailwindcss/tailwind.css';
 import './App.css';
 
 function Hello() {
   return (
     <div>
-      <div className="Hello">
-        <img width="200" alt="icon" src={icon} />
-      </div>
-      <h1>electron-react-boilerplate</h1>
-      <div className="Hello">
-        <a
-          href="https://electron-react-boilerplate.js.org/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              📚
-            </span>
-            Read our docs
-          </button>
-        </a>
-        <a
-          href="https://github.com/sponsors/electron-react-boilerplate"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="folded hands">
-              🙏
-            </span>
-            Donate
-          </button>
-        </a>
+      <div className="flex items-center justify-center h-screen">
+        <img className="bg-blue-500 h-12" src={icon} alt="icon" />
+        <h1 className="text-4xl font-bold text-center text-blue-500">
+          Hello World!
+        </h1>
       </div>
     </div>
   );
