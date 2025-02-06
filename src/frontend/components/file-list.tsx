4033e4267d93b87c7077d779c2f6c549a345871b
Chaman Rawat <chamanrawat.dev@gmail.com>
2025-02-06 15:37:13 +0530

    feat(ui): enhance FileList component loading, trash and cancel icons

--- src/frontend/components/file-list.tsx
+++ src/frontend/components/file-list.tsx
@@ -1,3 +1,5 @@
+import { Loader2, Trash2, X } from 'lucide-react';
+
 import {
   FileList as UIFileList,
   FileListDescription,
@@ -7,6 +9,9 @@ import {
   FileListItem,
   FileListName,
   FileListSize,
+  FileListDescriptionText,
+  FileListAction,
+  FileListDescriptionSeparator,
 } from '@/frontend/components/ui/file-list';
 
 interface FileListProps {
@@ -14,6 +19,8 @@ interface FileListProps {
 }
 
 export default function FileList({ files }: FileListProps) {
+  const isRenaming = true;
+
   return (
     <UIFileList>
       {files.map((file) => (
@@ -24,8 +31,28 @@ export default function FileList({ files }: FileListProps) {
               <FileListName>{file.name}</FileListName>
               <FileListDescription>
                 <FileListSize>{file.size}</FileListSize>
+
+                {isRenaming && (
+                  <>
+                    <FileListDescriptionSeparator />
+                    <FileListDescriptionText>
+                      <Loader2 className="size-3 animate-spin" />
+                      Renaming...
+                    </FileListDescriptionText>
+                  </>
+                )}
               </FileListDescription>
             </FileListInfo>
+            {isRenaming && (
+              <FileListAction>
+                <X />
+                <span className="sr-only">Close</span>
+              </FileListAction>
+            )}
+            <FileListAction>
+              <Trash2 />
+              <span className="sr-only">Remove</span>
+            </FileListAction>
           </FileListHeader>
         </FileListItem>
       ))}
