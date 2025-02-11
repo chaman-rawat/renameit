21c9a32ae1053edae72c242b922e482e7538874d
Chaman Rawat <chamanrawat.dev@gmail.com>
2025-02-11 15:16:11 +0530

    fix(ui): avoid unmounting components in HomeView Before it causing re rendering to child components and hence losing their states.

--- src/frontend/views/HomeView.tsx
+++ src/frontend/views/HomeView.tsx
@@ -9,25 +9,22 @@ import History from './HistoryView';
 export default function Home() {
   const [activePage, setActivePage] = useState('File');
 
-  const renderActivePage = () => {
-    switch (activePage) {
-      case 'Settings':
-        return <Settings />;
-      case 'History':
-        return <History />;
-      case 'About':
-        return <About />;
-      case 'File':
-      default:
-        return <FileDropzone />;
-    }
-  };
-
   return (
     <SidebarProvider>
       <AppSidebar activePage={activePage} setActivePage={setActivePage} />
       <main className="flex flex-col flex-1 gap-4 p-4">
-        {renderActivePage()}
+        <section className={activePage === 'File' ? 'block' : 'hidden'}>
+          <FileDropzone />
+        </section>
+        <section className={activePage === 'History' ? 'block' : 'hidden'}>
+          <History />
+        </section>
+        <section className={activePage === 'Settings' ? 'block' : 'hidden'}>
+          <Settings />
+        </section>
+        <section className={activePage === 'About' ? 'block' : 'hidden'}>
+          <About />
+        </section>
       </main>
     </SidebarProvider>
   );
